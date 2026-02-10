c3d4f78b7acebcddca3b31e331e858a63df2a823
Samadhan2025 <samadhan.birajdar25@gmail.com>
2026-02-10 19:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
-  // your code here
+  // your code hereif (str.length === 0) return "";
+
+  // remove leading spaces
+  s = s.trimStart();
+
+  const spaceIndex = s.indexOf(" ");
+
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  return s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
